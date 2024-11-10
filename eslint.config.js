d4149ef6031589a0aca2b0c0996db80eaa0baa7d
veronikaKul <veronika@intmax.io>
2024-11-10 14:45:07 +0100

    Added tseslint flat structure

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,32 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import importPlugin from 'eslint-plugin-import';
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: ['dist'],
+    languageOptions: {
+      // other options...
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "import": importPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +34,78 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      "sort-imports": [
+        "error",
+        {
+          "ignoreCase": true,
+          "ignoreDeclarationSort": true
+        }
+      ],
+      "import/order": [
+        "error",
+        {
+          "groups": ["builtin", "external", "internal"],
+          "pathGroups": [
+            {
+              "pattern": "react**",
+              "group": "external",
+              "position": "before"
+            },
+            {
+              "pattern": "@**",
+              "group": "external",
+              "position": "before"
+            },
+            {
+              "pattern": "icons",
+              "group": "internal",
+              "position": "after"
+            },
+            {
+              "pattern": "constants/**",
+              "group": "internal",
+              "position": "after"
+            },
+            {
+              "pattern": "entities",
+              "group": "internal",
+              "position": "after"
+            },
+            {
+              "pattern": "helpers/**",
+              "group": "internal",
+              "position": "after"
+            },
+            {
+              "pattern": "hooks/**",
+              "group": "internal",
+              "position": "after"
+            },
+
+            {
+              "pattern": "components/**",
+              "group": "internal",
+              "position": "after"
+            },
+            {
+              "pattern": "styles/**",
+              "group": "internal",
+              "position": "after"
+            },
+            {
+              "pattern": "theme/**",
+              "group": "internal",
+              "position": "after"
+            }
+          ],
+          "pathGroupsExcludedImportTypes": ["react"],
+          "newlines-between": "always",
+          "alphabetize": {
+            "order": "asc",
+            "caseInsensitive": true
+          },
+        },
+      ]
     },
   },
 )
